a1b0b840cc1c6c7bf72b629efc510e746fe2e3df
JohnKberg <john.kberg81@gmail.com>
2016-12-07 16:30:11 +0100

    Add simple Angular listing of Episodes

--- Minuten/App_Start/BundleConfig.cs
+++ Minuten/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace Minuten
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/angular.js",
                         "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
---
+++ Minuten/Scripts/minuten-app.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module("MinutenApp", []);
+
+app.controller("MinutenController", function ($scope, $http) {
+
+    $scope.episodes = [];
+    $scope.loading = false;
+    console.log("CTOR - episodes len: " + $scope.episodes.length)
+
+    $scope.listEpisodes = function () {
+
+        $scope.episodes = [];
+        $http.get("/api/episodes")
+        .success(function (data, status, headers, config) {
+            $scope.episodes = data;
+            $scope.loading = false;
+            console.log("API OK! episodes.len: " + $scope.episodes.length);
+        })
+        .error(function (data, status, headers, config) {
+            $scope.loading = false;
+            console.log("API ERROR!");
+        });
+    };
+
+});
